8d9a7acdd4865ae5c354b09169875886bc9b81bf
skhandelwal7 <shubham.khandelwal@vinsol.com>
2019-09-06 11:05:58 +0530

    Indentation

--- Alter_Table_Exercise.sql
+++ Alter_Table_Exercise.sql
@@ -6,7 +6,7 @@ roll_no VARCHAR(10)
 );
 
 ALTER TABLE testing_table
-DROP COLUMN  name,
+DROP COLUMN name,
 CHANGE COLUMN contact_name username VARCHAR(20) ,
 ADD COLUMN first_name VARCHAR(20),
 ADD COLUMN last_name VARCHAR(20),
