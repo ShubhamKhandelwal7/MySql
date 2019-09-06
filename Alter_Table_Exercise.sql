56ac899e3c2e1523435d6585c18bcd81d57af6b2
skhandelwal7 <shubham.khandelwal@vinsol.com>
2019-09-06 10:44:49 +0530

    Exercise-1

---
+++ Alter_Table_Exercise.sql
@@ -0,0 +1,15 @@
+CREATE TABLE testing_table
+(
+name VARCHAR(20),
+contact_name VARCHAR(20),
+roll_no VARCHAR(10)	
+);
+
+ALTER TABLE testing_table
+DROP COLUMN  name,
+CHANGE COLUMN contact_name username VARCHAR(20) ,
+ADD COLUMN first_name VARCHAR(20),
+ADD COLUMN last_name VARCHAR(20),
+MODIFY roll_no INTEGER;
+
+
