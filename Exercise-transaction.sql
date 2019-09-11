2c6e770e5412e1d655e1d8ca449c1885444dc5cf
skhandelwal7 <shubham.khandelwal@vinsol.com>
2019-09-11 12:42:00 +0530

    made ID as discrete identifier for table accounts splitted all three transactions

--- Exercise-transaction.sql
+++ Exercise-transaction.sql
@@ -10,10 +10,9 @@ PRIMARY KEY (ID,Account_no)
 
 CREATE TABLE accounts
 (
-ID INTEGER,
+ID INTEGER PRIMARY KEY,
 Account_no INTEGER,
 Balance INTEGER,
-FOREIGN KEY (ID) REFERENCES users(ID),
 FOREIGN KEY (Account_no) REFERENCES users(Account_no)
 );
 
@@ -38,23 +37,27 @@ VALUES (1,15001,2500),
 START TRANSACTION;
 SET  @deposit_amount=1000;
 UPDATE accounts
-JOIN users ON accounts.ID=users.ID
+JOIN users ON accounts.Account_no=users.Account_no
 SET accounts.balance = accounts.balance+@deposit_amount
 WHERE users.name='userA';
+COMMIT;
 
+START TRANSACTION;
 SET @withdrawl_amount=500;
 UPDATE accounts
-JOIN users ON accounts.ID=users.ID
+JOIN users ON accounts.Account_no=users.Account_no
 SET accounts.balance = accounts.balance-@withdrawl_amount
 WHERE users.name='userA';
+COMMIT;
 
+START TRANSACTION;
 SET @transfer_amount=200;
 UPDATE accounts
-JOIN users ON accounts.ID=users.ID
+JOIN users ON accounts.Account_no=users.Account_no
 SET accounts.balance = accounts.balance-@transfer_amount 
 WHERE users.name='userA';
 UPDATE accounts
-JOIN users ON accounts.ID=users.ID
+JOIN users ON accounts.Account_no=users.Account_no
 SET accounts.balance = accounts.balance+@transfer_amount
 WHERE users.name='userB';
 COMMIT;
