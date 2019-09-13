a2e13448a26c1d6bead91f1be070a2cba8fda0ec
skhandelwal7 <shubham.khandelwal@vinsol.com>
2019-09-13 15:39:51 +0530

    naming changes

--- Exercise-3-userTables.sql
+++ Exercise-3-userTables.sql
@@ -5,105 +5,107 @@ ID INTEGER AUTO_INCREMENT PRIMARY KEY,
 name VARCHAR(20),
 type VARCHAR(10)
 );
+CREATE TABLE categories
+(
+ID INTEGER AUTO_INCREMENT PRIMARY KEY,
+types VARCHAR(20)
+);
 CREATE TABLE articles
 (
 ID INTEGER AUTO_INCREMENT PRIMARY KEY, 
 content VARCHAR(20),
-users_ID INTEGER,
-categories_ID INTEGER,
-FOREIGN KEY (users_ID) REFERENCES users(ID),
-FOREIGN KEY (categories_ID) REFERENCES categories(ID)
+user_id INTEGER,
+category_id INTEGER,
+FOREIGN KEY (user_id) REFERENCES users(ID),
+FOREIGN KEY (category_id) REFERENCES categories(ID)
 );
-CREATE TABLE categories
-(
-ID INTEGER AUTO_INCREMENT PRIMARY KEY,
-category VARCHAR(20)
-);
+
 CREATE TABLE comments 
 (
 ID INTEGER AUTO_INCREMENT PRIMARY KEY,
-articles_ID INTEGER,
-comment VARCHAR(30),
-commented_by INTEGER,
-FOREIGN KEY (articles_ID) REFERENCES articles(ID),
-FOREIGN KEY (commented_by) REFERENCES users(ID)
+article_id INTEGER,
+remark VARCHAR(30),
+remarked_by INTEGER,
+FOREIGN KEY (article_id) REFERENCES articles(ID),
+FOREIGN KEY (remarked_by) REFERENCES users(ID)
 );
 
-INSERT INTO users
-VALUES (1,'user1','Admin'),
-(2,'user2','Normal'),
-(3,'user3','Normal'),
-(4,'user4','Normal');
+INSERT INTO users(name,type)
+VALUES ('user1','Admin'),
+('user2','Normal'),
+('user3','Normal'),
+('user4','Normal');
 
-INSERT INTO articles
-VALUES ('Amazon Fires',1),
-('ISRO Chandrayaan',2),
-('Polotical Dilemma',1),
-('US Open',3),
-('Addicting Cinema',4),
-('Gulf Crisis',4),
-('RPA Takedown',4);
+INSERT INTO categories(types)
+VALUES ('Nature'),
+('Science'),
+('Politics'),
+('Entertainment');
 
-INSERT INTO categories
-VALUES ('Amazon Fires','Nature'),
-('ISRO Chandrayaan','Science'),
-('Polotical Dilemma','Politics'),
-('US Open','Entertainment'),
-('Addicting Cinema','Entertainment'),
-('Gulf Crisis','Politics'),
-('RPA Takedown','Science');
+INSERT INTO articles(content,user_id,category_id)
+VALUES ('Amazon Fires',1,1),
+('ISRO Chandrayaan',2,2),
+('Polotical Dilemma',1,3),
+('US Open',3,4),
+('Addicting Cinema',4,4),
+('Gulf Crisis',4,3),
+('RPA Takedown',4,2),
+('NewsLetter',2,4);
 
-INSERT INTO comments
-VALUES ('Amazon Fires','Alarmng!!',3),
-('ISRO Chandrayaan','New Leap Forward',1),
-('US Open','great games',2),
-('Amazon Fires','Concerning',3),
-('ISRO Chandrayaan','great larnings',4),
-('Gulf Crisis','Unlooked',3),
-('RPA Takedown','New Beginning',2),
-('ISRO Chandrayaan','Promising',1);
+INSERT INTO comments(article_id,remark,remarked_by)
+VALUES (1,'Alarmng!!',3),
+(2,'New Leap Forward',1),
+(4,'great games',2),
+(1,'Concerning',3),
+(2,'great larnings',4),
+(6,'Unlooked',3),
+(7,'New Beginning',2),
+(2,'Promising',1);
 
-DROP TABLE users;
-DROP TABLE articles;
-DROP TABLE categories;
-DROP TABLE comments;
+UPDATE articles
+SET content='ISRO Chandrayaan II'  
+WHERE ID=2;
+DELETE FROM articles
+WHERE ID=8;
 
 -- select all articles whose author's name is user3 (Do this exercise using variable also). --
 SET @user_name='user3';
-SELECT paper 
-FROM articles WHERE author_ID =(
+SELECT content 
+FROM articles WHERE user_id =(
 SELECT ID FROM users 
 WHERE name=@user_name);
 
 -- For all the articles being selected above, select all the articles and also the comments associated with those articles in a single query --
-SELECT articles.paper, comments.comment
+SELECT articles.content, comments.remark
 FROM articles 
-JOIN comments ON articles.paper=comments.article
-WHERE articles.author_ID=(
+JOIN comments ON articles.id=comments.article_id
+WHERE articles.user_id IN(
 SELECT ID FROM users 
 WHERE name='user3');
 
-SELECT article, comment
-FROM comments WHERE article IN(
-SELECT paper FROM articles
-WHERE author_ID=(
+SELECT (SELECT content FROM articles WHERE id=article_id), remark
+FROM comments WHERE article_id IN(
+SELECT id FROM articles
+WHERE user_id=(
 SELECT ID FROM users 
 WHERE name='user3'));
 
 -- Write a query to select all articles which do not have any comments --
-SELECT paper FROM articles
-WHERE articles.paper NOT IN (
-SELECT article FROM comments);
+SELECT content FROM articles
+WHERE articles.id NOT IN (
+SELECT article_id FROM comments);
 
 -- Write a query to select article which has maximum comments --
-SELECT article from comments 
-GROUP BY article
-HAVING COUNT(article) >=all (
-select COUNT(article) from comments group by article);
+SELECT content FROM articles 
+WHERE id IN(
+SELECT article_id FROM comments 
+GROUP BY article_id
+HAVING COUNT(article_id) >=All (
+SELECT COUNT(article_id) FROM comments GROUP BY article_id));
 
 -- Write a query to select article which does not have more than one comment by the same user  --
 
-SELECT articles.paper FROM comments
-Left JOIN articles ON comments.article=articles.paper
-GROUP BY comments.article
-HAVING count(comments.commented_by)<=1;
+SELECT articles.content,count(comments.remarked_by) FROM comments
+Left JOIN articles ON comments.article_id=articles.id
+GROUP BY comments.article_id
+HAVING count( comments.remarked_by)<=1;
